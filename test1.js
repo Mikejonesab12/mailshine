ea737c2b11ab96b4d85337fea1263ad10983a733
Michael <mikejonesab1@yahoo.com>
2017-04-29 16:56:42 -0700

    Removed test files accidentally pushed previously.

--- test1.js
+++
@@ -1,7 +0,0 @@
-function test1 (){
-    this.props = {
-        var1: 'var1'
-    };
-}
-
-module.exports = test1;
--- test3.js
+++
@@ -1,9 +0,0 @@
-var test1 = require('./test1.js');
-var test2 = require('./test2.js');
-
-
-
-var ins = new test2();
-ins.changeProp();
-console.log(new test1().props.var1);
-console.log(ins.props.var1);
